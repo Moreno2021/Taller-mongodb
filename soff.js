62ec53ba27771ff707de9bf254df8bcb7ab2e2ea
Moreno2021 <127993114+Moreno2021@users.noreply.github.com>
2023-03-30 15:09:11 -0500

    soff.js
    soff.js

---
+++ soff.js
@@ -0,0 +1,78 @@
+# Taller-mongodb
+taller
+use('SOFF_Monda');
+
+
+
+// db.createCollection('clientes',{
+//   validator: {
+//     $jsonSchema:{
+//       properties:{
+//         nombre:{
+//           bsonType:"string"
+//         },
+//         apellido:{
+//           bsonType:"string"
+//         },
+//         edad:{
+//           bsonType:"string"
+//         },
+//         email:{
+//           bsonType:"string"
+
+//         },
+//         celular:{
+//           bsonType:"string"
+//         }
+//       }
+//     }
+//   }
+// })
+
+// db.createCollection('proveedores',{
+//   validator: {
+//     $jsonSchema:{
+//       properties:{
+//         nombre:{
+//           bsonType:"string"
+//         },
+//         apellido:{
+//           bsonType:"string"
+//         },
+//         edad:{
+//           bsonType:"int"
+//         },
+//         email:{
+//           bsonType:"string"
+//         },
+//         celular:{
+//           bsonType:"string"
+//         }
+//       }
+//     }
+//   }
+// })
+
+// db.createCollection('empleados',{
+//   validator: {
+//     $jsonSchema:{
+//       properties:{
+//         nombre:{
+//           bsonType:"string"
+//         },
+//         apellido:{
+//           bsonType:"string"
+//         },
+//         edad:{
+//           bsonType:"int"
+//         },
+//         email:{
+//           bsonType:"string"
+//         },
+//         celular:{
+//           bsonType:"string"
+//         }
+//       }
+//     }
+//   }
+// })
